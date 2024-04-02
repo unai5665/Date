a41118a30d8f51dd4304d1d0e016c87eb83e8962
unai5665 <unaiperez5665@gmail.com>
2024-04-02 09:10:00 +0200

    creado fichera prueba date

--- date.py
+++ date.py
@@ -111,21 +111,3 @@ class Date:
             return (self.year, self.month, self.day) == (other.year, other.month, other.day)
         return False
 
-fecha = Date(21, 3, 2024)
-print(Date.is_leap_year(2024))
-print(Date.days_in_month(3, 2024))
-print(fecha.get_delta_days())
-print(fecha.weekday)
-print(fecha.is_weekend)
-print(fecha.short_date)
-print(fecha)
-nueva_fecha = fecha + 10
-print(nueva_fecha.short_date)
-fecha_anterior = fecha - 3
-print(fecha_anterior.short_date)
-otra_fecha = Date(1, 1, 2024)
-diferencia = fecha - otra_fecha
-print(diferencia)
-print(otra_fecha < fecha)
-print(fecha > otra_fecha)
-print(otra_fecha == fecha)
