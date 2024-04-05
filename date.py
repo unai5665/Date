3604088097b55575b8ac86d6cfad79157bba224a
unai5665 <151141045+unai5665@users.noreply.github.com>
2024-04-05 17:59:29 +0200

    actualizado date.py

--- date.py
+++ date.py
@@ -29,85 +29,66 @@ class Date:
             return False
     @staticmethod
     def days_in_month(month: int, year: int) -> int:
-        days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        diasmes= [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         if Date.is_leap_year(year) and month == 2:
             return 29
         else:
-            return days[month-1]
+            return diasmes[month-1]
 
     def get_delta_days(self) -> int:
         '''Número de días transcurridos desde el 1-1-1900 hasta la fecha.'''
-        days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        dias_en_mes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-        
         delta_days = (self.year - 1900) * 365
-        for y in range(1900, self.year):
-            if self.is_leap_year(y):
+        for x in range(1900, self.year):
+            if self.is_leap_year(x):
                 delta_days += 1
 
-       
+        
         for m in range(1, self.month):
-            delta_days += days_in_month[m - 1]
+            delta_days += dias_en_mes[m - 1]
             if m == 2 and self.is_leap_year(self.year):
                 delta_days += 1
         delta_days += self.day - 1
 
         return delta_days
 
+    #En el metodo weekday de datetime hubo un problema y es que en este se representa el lunes con el 0 y el domingo con el 6, y el el test nos piden que el domingo sea 0 y el jueves sea 4 en vez de 3 con los cual he modificado el metodo. 
+
     @property
     def weekday(self) -> int:
         fecha = datetime(self.year, self.month, self.day)
-        return fecha.weekday()
+        weekday_orden = fecha.weekday()
+    
+        ajuste_weekday = (weekday_orden + 1) % 7
+
+        return ajuste_weekday
 
     @property
     def is_weekend(self) -> bool:
         '''Devuelve True si la fecha corresponde a un sábado, False de lo contrario.'''
         return self.weekday == 6
 
+    
     @property
     def short_date(self) -> str:
-        return "{:02d}/{:02d}/{:04d}".format(self.day, self.month, self.year)
+        dia = self.day
+        mes = self.month
+        if self.day < 10:
+            dia = "0"+ str(self.day)
+        if self.month < 10:
+            mes = "0"+ str(self.month)
+        return f"{dia}/{mes}/{self.year}"
+
 
     def __str__(self):
         '''MARTES 2 DE SEPTIEMBRE DE 2003'''
         weekdays = ["LUNES", "MARTES", "MIÉRCOLES", "JUEVES", "VIERNES", "SÁBADO", "DOMINGO"]
-        month_names = ["ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"]
-        day_of_week = datetime(self.year, self.month, self.day).weekday()
+        nombremes = ["ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"]
+        dias_semana = datetime(self.year, self.month, self.day).weekday()
         return f"{weekdays[day_of_week]} {self.day} DE {month_names[self.month - 1]} DE {self.year}"
 
     def __add__(self, days: int):
         '''Sumar un número de días a la fecha'''
         date_obj = datetime(self.year, self.month, self.day) + timedelta(days=days)
         return Date(date_obj.day, date_obj.month, date_obj.year)
-
-    def __sub__(self, other):
-        '''Dos opciones:
-        1) Restar una fecha a otra fecha -> Número de días
-        2) Restar un número de días de la fecha -> Nueva fecha'''
-        if isinstance(other, Date):
-            delta = datetime(self.year, self.month, self.day) - datetime(other.year, other.month, other.day)
-            return delta.days
-        elif isinstance(other, int):
-            date_obj = datetime(self.year, self.month, self.day) - timedelta(days=other)
-            return Date(date_obj.day, date_obj.month, date_obj.year)
-        else:
-            raise TypeError("Unsupported operand type(s) for -: 'Date' and '{}'".format(type(other)))
-
-    def __lt__(self, other):
-        '''Menor que (<)'''
-        if isinstance(other, Date):
-            return (self.year, self.month, self.day) < (other.year, other.month, other.day)
-        raise TypeError("Unsupported operand type(s) for <: 'Date' and '{}'".format(type(other)))
-
-    def __gt__(self, other):
-        '''Mayor que (>)'''
-        if isinstance(other, Date):
-            return (self.year, self.month, self.day) > (other.year, other.month, other.day)
-        raise TypeError("Unsupported operand type(s) for >: 'Date' and '{}'".format(type(other)))
-
-    def __eq__(self, other) -> bool:
-        '''Compara si dos fechas son iguales.'''
-        if isinstance(other, Date):
-            return (self.year, self.month, self.day) == (other.year, other.month, other.day)
-        return False
-
